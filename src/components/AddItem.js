adf75899026c2bfae6ff06535fe3a0ad4cbb58b3
Nicola Della Marina <nicola@pn.webformat.com>
2020-10-04 17:11:03 +0200

    Validazione

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,6 +4,7 @@ import { ITEMS_QUERY, CREATE_ITEM } from "../query";
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
 import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
@@ -11,6 +12,11 @@ import Drawer from '@material-ui/core/Drawer';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  buttons: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
   fab: {
     position: 'fixed',
     bottom: theme.spacing(2),
@@ -31,6 +37,7 @@ const useStyles = makeStyles((theme) => ({
 export function AddItem() {
   const [showForm, setShowForm] = React.useState(false);
   const [newItemName, setNewItemName] = React.useState("");
+  const [invalidForm, setInvalidForm] = React.useState({ invalid: false, message: "" });
   const [createItem, { loading }] = useMutation(CREATE_ITEM, {
     refetchQueries: [{ query: ITEMS_QUERY }],
     onCompleted: () => {
@@ -50,21 +57,36 @@ export function AddItem() {
             className={classes.addForm}
             onSubmit={(e) => {
               e.preventDefault();
-              createItem({ variables: { data: { name: newItemName } } });
+              if (newItemName !== "") {
+                createItem({ variables: { data: { name: newItemName } } });
+              } else {
+                setInvalidForm({
+                  invalid: true,
+                  message: "campo obbligatorio"
+                });
+              }
             }}
           >
-            <TextField id="new-todo" label="Todo text" variant="outlined"
+            <TextField id="new-todo" label="Attività" variant="outlined"
+              error={invalidForm.invalid}
+              helperText={invalidForm.message}
               type="text"
               disabled={loading}
               value={newItemName}
               onChange={(e) => setNewItemName(e.target.value)}
               className={classes.inputField}
-            /><br />
-            <Button variant="contained" color="primary"
-              disabled={loading} type="submit"
-              startIcon={<SaveIcon />}>
-              Save
+            />
+            <div className={classes.buttons}>
+              <Button variant="contained" color="primary"
+                disabled={loading} type="submit"
+                startIcon={<SaveIcon />}>
+                Salva
                 </Button>
+              <Button variant="contained" color="secondary"
+                disabled={loading} startIcon={<CancelIcon />} onClick={() => setShowForm(false)}>
+                Annulla
+              </Button>
+            </div>
           </form>
         </Container>
       </Drawer>
